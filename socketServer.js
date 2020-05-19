f83dde758cdcde12aefef36f473f7ad6808edfd4
Serhad Eymen ŞENCAN <serhad.sencan@std.izu.edu.tr>
2020-05-19 23:44:42 +0300

    ip

--- socketServer.js
+++ socketServer.js
@@ -3,8 +3,8 @@ const Client = require('./models/client');
 require('./db/mongoose')
 
 var socket = dgram.createSocket('udp4');
-//socket.bind(33333, '134.122.95.112');
-socket.bind(33333,'127.0.0.1');
+socket.bind(33333, '134.122.95.112');
+//socket.bind(33333,'127.0.0.1');
 //Type0: same nat type1: different nat type2: multiple nat 
 var connectionType=1;
 var publicEndpointA = null;
