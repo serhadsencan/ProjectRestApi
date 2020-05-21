88167c0b048ba4bdd1b82d6f1d912d001b75d12e
Serhad Eymen ŞENCAN <serhad.sencan@std.izu.edu.tr>
2020-05-22 02:43:08 +0300

    new collection

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/task-man-api',{
+mongoose.connect('mongodb://127.0.0.1:27017/p2pcardgame',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false
